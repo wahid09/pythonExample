fb9564d73137f081028d027bba9772f875c4bbca
wahid09 <wahid.ullah.cse@gmail.com>
2019-02-03 19:33:10 +0600

    Initial commit

---
+++ array.py
@@ -0,0 +1,5 @@
+import array as arr
+
+number_list = [1, 2, 3, 4, 5, 6, 7, 8]
+number_array = arr.array('i', number_list)
+print(number_array[:])
---
+++ largestnumber.py
@@ -0,0 +1,13 @@
+#Find largest one among three number.
+
+num1 = int(input("Enter your first number : "))
+num2 = int(input("Enter your second number : "))
+num3 = int(input("Enter your third number : "))
+if (num1>=num2) and (num1>=num3):
+    largest = num1
+elif (num2>=num1) and (num2>=num3):
+    largest = num2
+else:
+    largest = num3
+print("The largest number among", num1, ",", num2, "and", num3, "is", largest)
+
