4dba062da54a1209a04fb778d1c58c3ebfdff6c9
wahid09 <wahid.ullah.cse@gmail.com>
2020-05-12 10:22:02 +0600

    webscrap script

---
+++ webscrap/index.py
@@ -0,0 +1,14 @@
+from websc import wlog
+from websc import wscrap
+
+wlog.set_custom_log_info('html/error.log')
+
+news_scrap = wscrap.NewsScraper(wscrap.url_aj, wlog)
+
+#news_scrap.retrieve_webpage()
+#news_scrap.write_webpage_as_html()
+
+news_scrap.read_webpage_from_html()
+news_scrap.convert_data_to_bs4()
+#news_scrap.print_data()
+news_scrap.parse_soup_to_simple_html()
